edc5ed8faf9236dd403791e4c5a43e0852398126
Princess Itaya <taycornelius@att.net>
2019-12-10 18:13:33 -0500

    styling table with bootstrap

--- planner.js
+++ planner.js
@@ -1,2 +1,39 @@
-var timedate = document.getElementById("#currentDay");
 
+
+
+
+   function savePlan(){
+    
+    var inputSaver = $(".form-control").val();
+
+    console.log(localStorage.setItem("inputs", inputSaver)); 
+
+  var finalPlan = localStorage.getItem("inputs");
+
+   
+   $(".form-control").text(finalPlan);
+
+
+
+   }
+
+   $(".btn").click(savePlan());
+ 
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+//use create element input
+//parse local storage to save input
+//use create element for buttons
