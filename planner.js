91442a05b95cb8e1e156839a44c1c06a6f38a141
Princess Itaya <taycornelius@att.net>
2019-12-11 19:04:26 -0500

    functionality

--- planner.js
+++ planner.js
@@ -1,24 +1,148 @@
 
+let m = moment().format('MMMM Do YYYY, h:mm:ss a');   
+console.log(m);
+$("#currentDay").text(m);
+
+let h = moment().format('HH');
+
+var mainDiv = document.getElementById("container");
 
 
 
-   function savePlan(){
-    
-    var inputSaver = $(".form-control").val();
 
-    console.log(localStorage.setItem("inputs", inputSaver)); 
 
-  var finalPlan = localStorage.getItem("inputs");
+$(document).ready(function(){
+
+   var t = 9;
+
+   for (var i=0; i<9; i++){
+
+   var rows = $("<div>");
+
+   rows.addClass("row");
+
+   rows.css("border", "2px solid black");
+//trying to make the row coloring thingy work
+
+
+
+var x = parseInt(h.substring(0, 2))
+
+
+
+  if (t == x){
+     rows.addClass("present");
+  }
+  
+   if (t > x){
+      rows.addClass("future");
+   }
+
+   else{
+      rows.addClass("past");
+   }
+
+   $(mainDiv).append(rows);
+
+
+   var time = $("<div>");
+   
+
+   time.addClass("col-sm-2");
+
+   var para = $("<p>");
+
+   if (t >= 9) {
+      para.text(t + "AM");
+
+   }
+
+   if (t == 12){
+      para.text(t + "PM");
+
+   }
+
+   else {
+      para.text(t%12 + "PM");
+   }
+
+   rows.append(time);
+
+   time.append(para);
+
+   var textBoxdiv = $("<div>");
+
+   var inputSpot = $("<input>");
+
+   inputSpot.attr("type", "text");
+
+   inputSpot.attr("id", "plans" + i);
+
+
+   var local = localStorage.getItem("hour" + i); //set to hour
+
+   if (local){
+
+      inputSpot.val(local);
+   }
+
+   textBoxdiv.addClass("col-md-8");
+
+   textBoxdiv.append(inputSpot);
+
+   rows.append(textBoxdiv);
+
+   var saveBtn = $("<div>");
+
+   saveBtn.addClass("col-sm-2");
+
+   var inputBtn = $("<input>");
+
+   inputBtn.attr("type", "button");
+
+   inputBtn.attr("id", "save" + i);
+   
+   inputBtn.attr("onclick", "savePlan("+i +"   )");
+
+   var textStuff =  "#save" + i;
 
    
-   $(".form-control").text(finalPlan);
+
+   inputBtn.text("Save");
+
+   saveBtn.append(inputBtn);
+
+   rows.append(saveBtn);
+
+   
+
+
+
+
+t++;
+   }
+
+
+
+})
+
+
+
+
+   function savePlan(index ){
+    
+      
+  var certainText = document.getElementById("plans" + index);
+
+  certainText.value;
+
+  localStorage.setItem("hour" + index, certainText.value);
 
 
 
    }
 
-   $(".btn").click(savePlan());
- 
+
 
   
   
@@ -29,11 +153,8 @@
   
   
   
-
 
 
 
 
-//use create element input
-//parse local storage to save input
-//use create element for buttons
+
